d9270a74742250b8066f14940f47ed2c748bb866
fernandosenacruz <fernandosenacruz@gmail.com>
2022-06-01 15:16:41 -0300

    Update product.js, sale.js, and 2 more files...

---
+++ back-end/src/database/models/product.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    price: DataTypes.NUMBER,
+    urlImage: DataTypes.STRING
+  }, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'Product',
+  });
+  return Product;
+};
---
+++ back-end/src/database/models/salesproducts.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class SalesProducts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  SalesProducts.init({
+    saleId: DataTypes.NUMBER,
+    productId: DataTypes.NUMBER,
+    quantity: DataTypes.NUMBER
+  }, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'SalesProducts',
+  });
+  return SalesProducts;
+};

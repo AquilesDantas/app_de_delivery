0048abd8f5fb109d937ab819111826fbb1e5093c
fernandosenacruz <fernandosenacruz@gmail.com>
2022-06-01 15:16:49 -0300

    Update 20220601180440-user.js, 20220601180649-product.js, and 2 more files...

---
+++ back-end/src/database/seeders/20220601180649-product.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: async (queryInterface) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkInsert('products', [
+      {
+        name: 'Delivery App Admin',
+        email: 'adm@deliveryapp.com',
+        password: 'a4c86edecc5aee06eff8fdeda69e0d04', // secret_admin
+        role: 'administrator',
+      },
+      {
+        name: 'Fulana Pereira',
+        email: 'fulana@deliveryapp.com',
+        password: '3c28d2b0881bf46457a853e0b07531c6', // secret_admin
+        role: 'seller',// secret_user
+      },
+      {
+        name: 'Cliente Zé Birita',
+        email: 'zebirita@email.com',
+        password: '1c37466c159755ce1fa181bd247cb925', // secret_admin
+        role: 'customer',// secret_user
+      },
+    ], { timeStamps: false });
+  },
+  down: async (queryInterface) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
+

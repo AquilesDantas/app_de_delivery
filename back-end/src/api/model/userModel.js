3fd5bc30e2d7f242f5ee2ec78ee9cad1737fe551
Lucas Toledo <lucasmoraisss@gmail.com>
2022-06-02 15:49:40 -0300

    Test: User Model - findOne

--- back-end/src/api/model/userModel.js
+++ back-end/src/api/model/userModel.js
@@ -2,10 +2,8 @@ const User = require('../../database/models/user');
 
 class UserModel {
   static async findOne(email) {
-    console.log(`aquiiiii----${email}`);
-    const use = await User.findOne({ where: { email } });
-    console.log(`teste ----${use}`);
-    return use;
+    const user = await User.findOne({ where: { email } });
+    return user;
   }
 }
 

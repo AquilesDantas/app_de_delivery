1ad709459d600a452cc62f64c7ff26f2d6ecb0dc
Igor Campos Moreira <igorcampos200@gmail.com>
2022-06-07 13:05:59 -0300

    bug: cards

--- front-end/src/components/ProductsList.jsx
+++ front-end/src/components/ProductsList.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Button } from 'react-bootstrap';
 import { getProducts } from '../API/Request';
+import './ProductsList.css';
 
 const ProductsList = () => {
   const token = useSelector(({ data }) => data.token.payload);
@@ -11,11 +13,34 @@ const ProductsList = () => {
       setProducts(await getProducts(token));
     };
     ximira();
-  }, []);
+  }, [token]);
 
-  console.log(products.data);
+  const cards = (data) => (
+    <div className="card" key={ data.id }>
+      <section className="card-image">
+        <img src={ data.urlImage } alt={ data.name } />
+      </section>
+      <section className="data-container">
+        <h5>{data.name}</h5>
+        <h6>{data.price}</h6>
+        <Button>
+          +
+        </Button>
+        <Button>
+          -
+        </Button>
+      </section>
+    </div>
+  );
+
+  const cardList = () => products.data.map((product) => cards(product));
+  if (!products.data) {
+    return <p>Loading</p>;
+  }
   return (
-    <p>aqui</p>
+    <div className="container-card">
+      {cardList()}
+    </div>
   );
 };
 
